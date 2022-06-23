8948dd9a9eaeafcc0acf82a5fd52f4f54120f79d
Marianne <mariannecowherd@gmail.com>
2022-06-22 17:20:10 -0700

    clean for savio

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-## file to define values and folders
-import sys
-
-homedir = sys.path[0]
-#homedir = '/global/home/users/cowherd/snow_fires/'
-datadir = homedir + '/data'
-rawdatadir = '/global/scratch/users/cowherd/'
+## file to define values
